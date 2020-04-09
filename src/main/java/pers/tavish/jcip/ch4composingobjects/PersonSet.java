9f23bc524ba4e6e6c0580efdeebe45c3c2190297
Tavish <bit_zjm@hotmail.com>
2020-04-09 18:03:04 +0800

    update "202004091802"

---
+++ src/main/java/pers/tavish/jcip/ch4composingobjects/PersonSet.java
@@ -0,0 +1,25 @@
+package pers.tavish.jcip.ch4composingobjects;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// 程序清单4-2
+@ThreadSafe
+public class PersonSet {
+    @GuardedBy("this")
+    private final Set<Person> mySet = new HashSet<>();
+
+    public synchronized void addPerson(Person p) {
+        mySet.add(p);
+    }
+
+    public synchronized boolean containsPerson(Person p) {
+        return mySet.contains(p);
+    }
+
+    interface Person {
+    }
+}
