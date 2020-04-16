aad1262a476fbdac5bbea313890784d0b9a48f92
Tavish <bit_zjm@hotmail.com>
2020-04-16 18:19:28 +0800

    update 202004161819

---
+++ src/main/java/pers/tavish/jcip/ch5buildingblocks/UnsafeVectorHelpers.java
@@ -0,0 +1,16 @@
+package pers.tavish.jcip.ch5buildingblocks;
+
+import java.util.Vector;
+
+// 程序清单5-1
+public class UnsafeVectorHelpers {
+    public static Object getLast(Vector list) {
+        int lastIndex = list.size() - 1;
+        return list.get(lastIndex);
+    }
+
+    public static void deleteLast(Vector list) {
+        int lastIndex = list.size() - 1;
+        list.remove(lastIndex);
+    }
+}
